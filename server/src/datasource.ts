af8bcd1cfb5111e187c46e612f9f7f5484bc8643
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-11-27 21:32:49 +0200

    Add datasource, users table migration @ server

---
+++ server/src/datasource.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+	type: "mysql",
+	host: "localhost",
+	port: Number(process.env.MYSQL_PORT),
+	username: String(process.env.MYSQL_USER),
+	password: String(process.env.MYSQL_PASSWORD),
+	database:  String(process.env.MYSQL_DATABASE),
+	synchronize: false,
+	logging: true,
+	entities: ["models/*.ts", "models/**/*.ts"],
+	subscribers: [],
+	migrations: ["src/migrations/*.ts"],
+})
---
+++ server/src/migrations/1701021921852-create_users_table.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface,
+  QueryRunner,
+  Table,
+  TableIndex,
+  TableColumn,
+  TableForeignKey
+} from "typeorm"
+
+export class CreateUsersTable1701021921852 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: "users",
+          columns: [
+            {
+              name: 'id',
+              type: "int",
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment'
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+            },
+            {
+              name: 'email',
+              type: 'varchar',
+              isUnique: true
+
+            },
+            {
+              name: "password",
+              type: "varchar"
+            },
+            {
+              name: "lastname",
+              type: "varchar"
+            },
+            {
+              name: "created_at",
+              type: "timestamp",
+              default: "now()"
+            },
+            {
+              name: 'updated_at',
+              type: "datetime",
+              default: "CURRENT_TIMESTAMP",
+              onUpdate: "CURRENT_TIMESTAMP"
+            }
+          ]
+        })
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+			await queryRunner.dropTable('users', true);
+		}
+}
