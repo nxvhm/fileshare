4c2fda0a4e831d06afef578e9ca58bf1f6659bcc
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-11-29 22:59:25 +0200

    init routing @ client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './screens/Login.tsx';
+import Signup from './screens/Signup.tsx';
+import Home from './screens/Home.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+	{
+		path: "/login",
+		element: <Login />
+	},
+	{
+		path: "/signup",
+		element: <Signup />
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
