e3edb172ee0db36e049298e76707e4836b2c9cda
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-11-29 23:05:03 +0200

    use env port @ server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
+import "dotenv/config"
+import "reflect-metadata"
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello TS');
 });
-  
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
