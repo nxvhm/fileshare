52d719891e89ec8b47dff52adee120aca3c6956d
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-11-30 21:57:26 +0200

    Use auth controllers @ server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import "dotenv/config"
 import "reflect-metadata"
+import bodyParser from "body-parser";
 import express, { Request, Response } from 'express';
+import AuthController from './controllers/Auth';
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use('/auth', AuthController);
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello TS');
 });
