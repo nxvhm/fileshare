1486cac774449c0dd5a6a7271791766ba05c0125
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-12-03 19:50:07 +0200

    Error handling @ client/Auth

--- client/src/services/Auth.ts
+++ client/src/services/Auth.ts
@@ -12,8 +12,12 @@ axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 export const signupRequest = (data:signupData) => {
 	return new Promise((resolve, reject) => {
 		axios.post('/auth/signup', data).then(
-			res => res.status == 200 ? resolve({success: true}) : reject({success:false, message: res.data?.message})
-		).catch(e => reject({success: false, message: e.response.data?.message}));
+			res => res.status == 200
+				? resolve({success: true})
+				: reject({success:false, message: res.data?.message})
+		).catch(
+			e => reject({success: false, message: e.response?.data?.message ?? e.message})
+		);
 	});
 }
 
