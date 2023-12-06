657c9fe9fa29f9c43b9005370ad4ae06b344f628
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-12-06 23:40:11 +0200

    Login request

--- client/src/services/Auth.ts
+++ client/src/services/Auth.ts
@@ -7,16 +7,27 @@ type signupData = {
 	password_repeat: string
 }
 
+type loginData = {
+	email: string,
+	password: string
+}
+
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-export type AuthResponse = {
+export type SignupResponse = {
 	success: boolean,
 	message: undefined|string
 }
 
-export const signupRequest = (data:signupData):Promise<AuthResponse> => {
+export type LoginResponse = {
+	token: undefined|string,
+	message?: string
+}
+
+export const signupRequest = (data:signupData):Promise<SignupResponse> => {
 	return new Promise((resolve, reject) => {
-		axios.post('/auth/signup', data).then(
+		axios.post('/auth/signup', data)
+		.then(
 			res => res.status == 200
 				? resolve({success: true, message: "Signup Successfull"})
 				: reject(new Error(res.data?.message))
@@ -24,6 +35,19 @@ export const signupRequest = (data:signupData):Promise<AuthResponse> => {
 			e => reject(new Error(e.response?.data?.message ?? e.message))
 		);
 	});
+}
+
+export const loginRequest = (data:loginData):Promise<LoginResponse> => {
+	return new Promise((resolve, reject) => {
+		axios.post('/auth/login', data)
+		.then(
+			res => res.status == 200 && res.data.token
+				? resolve({token: String(res.data.token)})
+				: reject(new Error(res.data?.message))
+		).catch(
+			e => reject(new Error(e.response?.data?.message ?? e.message))
+		)
+	})
 }
 
 
