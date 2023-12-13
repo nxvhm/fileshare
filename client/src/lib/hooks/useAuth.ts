ed535bae60b2ea87cc142c8b2fcd3b5644aa93ec
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-12-13 22:54:27 +0200

    Add useAuth hook

---
+++ client/src/lib/hooks/useAuth.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import {Token, UserTokenData} from "../Token";
+import {AuthContextData} from "../context/AuthContext";
+
+export function useAuth():AuthContextData {
+	const [user, setUser] = useState(():UserTokenData|null => null);
+
+	useEffect(
+		() => setUser(Token.getData()),
+		[Token.isExpired()]
+	);
+
+	const loginUser = (token:string): void => {
+		Token.set(token);
+		setUser(Token.getData());
+	}
+
+	const logoutUser = (): void => {
+		setUser(null);
+		Token.remove();
+	}
+
+	return {user, loginUser, logoutUser}
+}
