bae2b342336c146ee77ee6238cea29b1baa9f3a3
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2023-12-14 21:48:43 +0200

    test router links

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home() {
 	return (
 		<>
 			<div>Home page</div>
+			<p><Link to={'/login'}>Login</Link></p>
+			<p><Link to={'/signup'}>Signup</Link></p>
 		</>
 	)
 }
