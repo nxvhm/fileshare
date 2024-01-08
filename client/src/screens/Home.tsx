d5dfc3cf350902b875e70e09e17833994cf8476c
nxvhm <a.partadzhiev@gmail.com>
2024-01-08 23:51:21 +0200

    Add empty toolbar

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -3,6 +3,19 @@ import AuthContext from "../lib/context/AuthContext";
 import { Button, Box, Container } from "@mui/material";
 import Drawer from '../components/main/Drawer';
 import Topbar from "../components/main/Topbar";
+import { styled } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+
+const HiddenFileInput = styled('input')({
+	height: 1,
+	overflow: 'hidden',
+	position: 'absolute',
+	bottom: 0,
+	left: 0,
+	width: 1,
+})
+
+
 function Home() {
 	const {user, logoutUser} = useContext(AuthContext);
 
@@ -15,6 +28,7 @@ function Home() {
         backgroundColor: theme => theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
 				flexGrow: 1, height: '100vh', overflow: 'auto'}}
 			>
+			<Toolbar />
 			<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
 				<div>Welcome {user?.name}</div>
 				<p><Button variant="contained" onClick={() => logoutUser()}>Logout</Button></p>
