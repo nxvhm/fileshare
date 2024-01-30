8d95907be8c032bed31a4dd9f986676a927ef717
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-01-30 22:24:49 +0200

    USe auth middleware when uploading

--- server/src/controllers/Upload.ts
+++ server/src/controllers/Upload.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import multer from 'multer';
+import AuthMiddleware from "../middleware/AUth";
+import { IUserAuthRequest } from "../definitions";
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' })
 
-router.post('/file', upload.single('file'), async(req: express.Request, res: express.Response) => {
-	console.log("dasdas", req.file);
+router.use(AuthMiddleware);
+
+router.post('/file', upload.single('file'), async(req: IUserAuthRequest, res: express.Response) => {
+	console.log("dasdas", req.user);
+	res.send({success: true});
 })
 
 
