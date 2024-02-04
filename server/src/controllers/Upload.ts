1e9f5bb8f297cd2fa7ac2fc6a2d34ce8a809aacc
nxvhm <a.partadzhiev@gmail.com>
2024-02-04 22:23:41 +0200

    Use files helper

--- server/src/controllers/Upload.ts
+++ server/src/controllers/Upload.ts
@@ -5,7 +5,7 @@ import { IUserAuthRequest } from "../definitions";
 import fs from 'fs';
 import { sep } from "path";
 import path from 'path';
-
+import { Files } from "../lib/Files";
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' })
@@ -24,18 +24,15 @@ router.post('/file', upload.single('file'), async(req: IUserAuthRequest, res: ex
 	const __dirname = path.resolve();
 	const filesDir = __dirname+sep+"files";
 
-	try {
-		await fs.promises.access(req.file.destination, fs.promises.constants.W_OK | fs.promises.constants.R_OK);
-	} catch (error) {
-		console.error(error);
-		return res.status(422).send("File not uploaded or not readable");
-	}
+	const exists = await Files.exists(req.file.destination);
+	if(!exists)
+		return res.status(500).send("File not uploaded or not readable");
 
-	try {
-		const createDir = await fs.promises.mkdir(filesDir, { recursive: true });
-
-	} catch (error) {
-		console.log("createDir error", error);
+	const filesDirExists = await Files.exists(Files.getUserFilesDirPath());
+	if(!filesDirExists) {
+		const filesDirCreated = await Files.mkdir(Files.getUserFilesDirPath());
+		if(!filesDirCreated)
+			return res.status(500).send("Files directory not available at the moment");
 	}
 
 	const source = fs.createReadStream(req.file.path);
