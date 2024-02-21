bc74634bdec43a0b82fa931888cf735726ba2fd5
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-02-21 23:10:34 +0200

    Separate file for definitions

---
+++ client/src/definitions.ts
@@ -0,0 +1,15 @@
+export type FileModel = {
+	id: number|null,
+	user_id: number|null,
+	name: string,
+	parent_id: number|null,
+	hash: string|null,
+	mime: string|null,
+	created_at: string|null
+}
+
+export type UserTokenData = {
+	id: number,
+	name: string,
+	email: string
+ }
