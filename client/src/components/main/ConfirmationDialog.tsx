4a4d427cfe076ef8cb3d8fdeb86a80d2d8e008f0
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-02-21 23:11:39 +0200

    Confirmation dialog component

---
+++ client/src/components/main/ConfirmationDialog.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export type ConfirmationDialogProps = {
+	isOpen: boolean,
+	dialogText: string,
+	cancelText?: string,
+	confirmText?: string,
+	onConfirm: () => void,
+	onCancel: () => void,
+	secondaryDialogText?: string
+}
+
+export default function ConfirmationDialog(props: ConfirmationDialogProps) {
+  const [open, setOpen] = useState(props.isOpen);
+
+	useEffect(
+		() => setOpen(props.isOpen),
+		[props.isOpen]
+	)
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{props.dialogText}</DialogTitle>
+				{props?.secondaryDialogText &&
+        	<DialogContent>
+          	<DialogContentText id="alert-dialog-description">{props?.secondaryDialogText}</DialogContentText>
+					</DialogContent>
+				}
+        <DialogActions>
+          <Button onClick={props.onCancel}>{props.cancelText ? props.cancelText : 'Cancel'}</Button>
+          <Button onClick={props.onConfirm}>{props.confirmText ? props.confirmText : 'Confirm'}</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
