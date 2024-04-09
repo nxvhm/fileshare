1c4fe80535c55047cda82bb58e32a147fe55d799
nxvhm <a.partadzhiev@gmail.com>
2024-04-09 21:03:38 +0300

    Show file details fn

--- client/src/lib/context/OpenFileDetailsContext.tsx
+++ client/src/lib/context/OpenFileDetailsContext.tsx
@@ -5,10 +5,10 @@ import { FileDetailsDrawerContextType, FileModel } from "../../definitions";
 const defaultState = {
 	drawerOpen: false,
 	selectedFile: null,
-	toggleDrawer: () => {}
+	toggleDrawer: () => {},
 }
 
-const OpenFileDetailsContext = createContext<FileDetailsDrawerContextType>(defaultState);
+const OpenFileDetailsContext = createContext<FileDetailsDrawerContextType>(defaultState as FileDetailsDrawerContextType);
 
 export const OpenFileDetailsContextProvider = ({children}: PropsWithChildren<{}>) => {
 
@@ -16,9 +16,13 @@ export const OpenFileDetailsContextProvider = ({children}: PropsWithChildren<{}>
 	const [selectedFile, setSelectedFile] = useState<FileModel|null>(null);
 
 	const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+	const showFileDetails = (file: FileModel) => {
+		setSelectedFile(file);
+		setDrawerOpen(true);
+	}
 
 	return(
-		<OpenFileDetailsContext.Provider value={{drawerOpen, toggleDrawer, selectedFile, setSelectedFile}}>
+		<OpenFileDetailsContext.Provider value={{drawerOpen, toggleDrawer, selectedFile, setSelectedFile, showFileDetails}}>
 			{children}
 		</OpenFileDetailsContext.Provider>
 	)
