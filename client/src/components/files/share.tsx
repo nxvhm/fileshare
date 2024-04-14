08db63121c05659e9240d7a0c97dc4ad40686d09
nxvhm <a.partadzhiev@gmail.com>
2024-04-14 22:28:56 +0300

    Share files [wip]

--- client/src/components/files/share.tsx
+++ client/src/components/files/share.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FileModel } from "../../definitions";
-
-import {Dialog, DialogTitle, DialogContent, DialogActions} from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Autocomplete, { AutocompleteInputChangeReason } from '@mui/material/Autocomplete';
+import {Dialog, DialogTitle, DialogContent, DialogActions, Button} from '@mui/material';
+import { SyntheticEvent } from "react";
+import { searchUsers, UserSearchResult } from "../../api/Files";
 
 export type ShareProps = {
 	open: boolean
@@ -11,17 +14,42 @@ export type ShareProps = {
 
 export default function Share(props: ShareProps) {
   const { open, file, onClose } = props;
+	const [userResults, setUserResults] = useState<UserSearchResult[]>([]);
+	const [selectedUser, setSelectedUser] = useState<UserSearchResult|null>(null);
+
+	const onSearch = (e: SyntheticEvent, value: string, reason: AutocompleteInputChangeReason) => {
+		if(value.length < 3)
+			return;
+
+		searchUsers(value).then(result => {
+			if(!result.length)
+					return;
+
+			setUserResults(result);
+			console.log('search users res:', result);
+		});
+	}
+
+	const shareWithSelectedUser = () => {
+		console.log('share with', selectedUser);
+	}
 
 	return(
-		<Dialog open={open} maxWidth={'xs'} fullWidth={true}
-			aria-labelledby="alert-dialog-title"
-			aria-describedby="alert-dialog-description"
-			onClose={() => onClose()}
-			>
+		<Dialog open={open} maxWidth={'sm'} fullWidth={true} onClose={() => onClose()} scroll="body">
 			<DialogTitle id="alert-dialog-title" textAlign={'center'}>Share {file?.name}</DialogTitle>
-			<DialogContent>
-				<p>dasdasdasdasdsa</p>
+			<DialogContent sx={{paddingTop: 3}}>
+					<Autocomplete id="combo-box-demo"
+						getOptionLabel={el => el.name}
+						options={userResults} sx={{ width: 'auto' }}
+						renderInput={(params) => <TextField {...params} label="Share with..." />}
+						onInputChange={onSearch}
+						onChange={(event, value) => setSelectedUser(value as UserSearchResult)}
+					/>
 			</DialogContent>
+			<DialogActions>
+				<Button variant="outlined" color="primary" onClick={shareWithSelectedUser}>Share with user</Button>
+				<Button variant="outlined" color="error">Cancel</Button>
+			</DialogActions>
 		</Dialog>
 	)
 
