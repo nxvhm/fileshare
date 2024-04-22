4b4198e55709a0074a368440377e6b24240f28e8
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-04-22 23:27:42 +0300

    Add relations to share model

--- server/src/models/Share.ts
+++ server/src/models/Share.ts
@@ -1,13 +1,27 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn , OneToOne, JoinColumn} from "typeorm";
 import { UserTokenData } from "../definitions";
 import "reflect-metadata"
+import { File } from "@/models/File";
+import { User } from "@/models/User";
 
 
 @Entity("shares")
 export class Share {
+
 	@PrimaryColumn("int")
 	user_id!: number;
 
 	@PrimaryColumn("int")
 	file_id!: number;
+
+	@OneToOne(() => User)
+	@JoinColumn({name: 'user_id'})
+	user!: User
+
+	@Column("timestamp")
+	created_at!: string
+
+	@OneToOne(() => File)
+	@JoinColumn({name: 'file_id'})
+	file!: File
 }
