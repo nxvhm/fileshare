237fc518518d569707771e34de3e261349e88b3d
nxvhm <a.partadzhiev@gmail.com>
2024-05-17 22:54:18 +0300

    Save onFileUpdate callback function in the state of FileDetailsDrawer context provider

--- client/src/lib/context/OpenFileDetailsContext.tsx
+++ client/src/lib/context/OpenFileDetailsContext.tsx
@@ -1,5 +1,5 @@
 import { useState, createContext, PropsWithChildren } from "react";
-import { FileDetailsDrawerContextType, FileModel } from "../../definitions";
+import { FileDetailsDrawerContextType, FileModel, filePropUpdateHandler } from "../../definitions";
 
 const defaultState = {
 	drawerOpen: false,
@@ -13,15 +13,16 @@ export const OpenFileDetailsContextProvider = ({children}: PropsWithChildren<{}>
 
 	const [drawerOpen, setDrawerOpen] = useState(false);
 	const [selectedFile, setSelectedFile] = useState<FileModel|null>(null);
-
 	const toggleDrawer = () => setDrawerOpen(!drawerOpen);
-	const showFileDetails = (file: FileModel) => {
+	const [onFileChange, setOnFileChange] = useState<filePropUpdateHandler>(null);
+	const showFileDetails = (file: FileModel, onFilePropChange: filePropUpdateHandler) => {
+		setOnFileChange(() => onFilePropChange);
 		setSelectedFile(file);
 		setDrawerOpen(true);
 	}
 
 	return(
-		<OpenFileDetailsContext.Provider value={{drawerOpen, toggleDrawer, selectedFile, setSelectedFile, showFileDetails}}>
+		<OpenFileDetailsContext.Provider value={{drawerOpen, toggleDrawer, selectedFile, setSelectedFile, showFileDetails, onFileChange, setOnFileChange}}>
 			{children}
 		</OpenFileDetailsContext.Provider>
 	)
