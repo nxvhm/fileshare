1f4030b3e87cefc08bd940b74294599b625cceb4
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-07-07 21:20:29 +0300

    Full width layout

--- client/src/components/main/Layout.tsx
+++ client/src/components/main/Layout.tsx
@@ -1,26 +1,24 @@
-import { useContext } from "react";
-import { Button, Box, Container, Toolbar} from "@mui/material";
+import { Box, Container, Toolbar} from "@mui/material";
 import { Toaster } from 'react-hot-toast';
-
 import Topbar from "./Topbar";
 import Drawer from "./Drawer";
 import FileDetailsDrawer from "./FileDetailsDrawer";
-import AuthContext from "../../lib/context/AuthContext";
 
 export default function Layout({children}: {children: React.ReactNode}) {
-	const {logoutUser} = useContext(AuthContext);
 
 	return(
 		<Box sx={{ display: 'flex' }}>
 		<Topbar />
 		<Drawer />
 		<FileDetailsDrawer></FileDetailsDrawer>
-		<Box component="main" sx={{
-			backgroundColor: theme => theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
-			flexGrow: 1, height: '100vh', overflow: 'auto'}}
+		<Box component="main"
+			sx={{
+				backgroundColor: theme => theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
+				flexGrow: 1, height: '100vh', overflow: 'auto'
+			}}
 		>
 		<Toolbar />
-		<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+		<Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
 			{children}
 		</Container>
 		</Box>
