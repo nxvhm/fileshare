7fca660eea84dceb1b3e3f894bb1993c3c8b09e7
nxvhm <a.partadzhiev@gmail.com>
2024-07-12 15:29:59 +0300

    Separate file for styled components for files

---
+++ client/src/components/files/styled.ts
@@ -0,0 +1,23 @@
+import { styled } from '@mui/system';
+import {TableRow} from '@mui/material';
+
+const VisuallyHiddenInput = styled('input')({
+	clip: 'rect(0 0 0 0)',
+	clipPath: 'inset(50%)',
+	height: 1,
+	overflow: 'hidden',
+	position: 'absolute',
+	bottom: 0,
+	left: 0,
+	whiteSpace: 'nowrap',
+	width: 1,
+});
+
+const FileTableRow = styled(TableRow)({
+	'&:hover': {
+		background: "#dbdbdb",
+		cursor: 'pointer'
+	}
+})
+
+export {FileTableRow, VisuallyHiddenInput}
