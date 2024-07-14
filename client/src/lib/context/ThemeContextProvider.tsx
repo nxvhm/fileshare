13a0181fdd68f6e419bd5580c4313f97eedbc360
nxvhm <a.partadzhiev@gmail.com>
2024-07-14 20:08:31 +0300

    Theme Context

---
+++ client/src/lib/context/ThemeContextProvider.tsx
@@ -0,0 +1,38 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { createContext, PropsWithChildren, useState } from "react";
+
+export type ThemesList = 'dark' | 'light';
+export type ThemeContextType = {
+  switchMode: (theme: ThemesList) => void,
+	mode: ThemesList,
+};
+
+const ThemeContext = createContext<ThemeContextType>({
+  switchMode: (mode: ThemesList) => {},
+	mode: 'light'
+});
+
+export const ThemeContextProvider = ({children}: PropsWithChildren<{}>) => {
+	const defaultMode: ThemesList = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+		? 'light'
+		: 'light'
+
+	const [mode, setMode] = useState<ThemesList>(defaultMode);
+	const switchMode = (theme: ThemesList) => {
+		setMode(theme);
+	}
+
+	const theme = createTheme({
+		palette: { mode }
+	})
+
+	return(
+		<ThemeContext.Provider value={{mode, switchMode }}>
+			<ThemeProvider theme={theme}>
+				{children}
+			</ThemeProvider>
+		</ThemeContext.Provider>
+	)
+}
+
+export default ThemeContext
