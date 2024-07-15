4ba7fb97f31af5056c6063862c99e045d555b0c9
nxvhm <a.partadzhiev@gmail.com>
2024-07-15 22:28:07 +0300

    Breadcrumbs styled components in separate file; Add theme dependant styles

--- client/src/components/files/breadcrumbs.tsx
+++ client/src/components/files/breadcrumbs.tsx
@@ -7,27 +7,11 @@ import {Home as HomeIcon, Folder as FolderIcon} from '@mui/icons-material';
 
 import { getBreadcrumbs } from '../../api/Files';
 import { FileModel } from '../../definitions';
+import { StyledBreadcrumb } from './styled';
 
 export type BreadcrumbsProps = {
 	folderId?: number|undefined
 }
-
-const StyledBreadcrumb = styled(Chip)(({ theme }) => {
-  const backgroundColor = theme.palette.grey[200];
-  return {
-    backgroundColor,
-    height: theme.spacing(3),
-    color: theme.palette.text.primary,
-    fontWeight: theme.typography.fontWeightRegular,
-    '&:hover, &:focus': {
-      backgroundColor: emphasize(backgroundColor, 0.06),
-    },
-    '&:active': {
-      boxShadow: theme.shadows[1],
-      backgroundColor: emphasize(backgroundColor, 0.12),
-    },
-  };
-}) as typeof Chip;
 
 function handleClick(event: React.MouseEvent<Element, MouseEvent>) {
   event.preventDefault();
--- client/src/components/files/styled.ts
+++ client/src/components/files/styled.ts
@@ -1,7 +1,9 @@
-import { styled } from '@mui/system';
-import {TableRow} from '@mui/material';
+import { styled, emphasize } from '@mui/material/styles';
+import {Icon, TableRow} from '@mui/material';
+import {Chip} from '@mui/material';
+import {Home as HomeIcon, Folder as FolderIcon} from '@mui/icons-material';
 
-const VisuallyHiddenInput = styled('input')({
+export const VisuallyHiddenInput = styled('input')({
 	clip: 'rect(0 0 0 0)',
 	clipPath: 'inset(50%)',
 	height: 1,
@@ -13,11 +15,30 @@ const VisuallyHiddenInput = styled('input')({
 	width: 1,
 });
 
-const FileTableRow = styled(TableRow)({
+export const FileTableRow = styled(TableRow)( ({theme}) => ({
 	'&:hover': {
-		background: "#dbdbdb",
+		background: theme.palette.mode == 'light' ? '#dbdbdb' : '#3a3a3a',
 		cursor: 'pointer'
 	}
-})
+}))
 
-export {FileTableRow, VisuallyHiddenInput}
+export const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+  return {
+    backgroundColor: theme.palette.mode == 'light' ? theme.palette.grey[200] : theme.palette.primary.main,
+    height: theme.spacing(3),
+    color: theme.palette.mode == 'light' ? theme.palette.text.primary : theme.palette.grey[900],
+		fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: emphasize(theme.palette.grey[200], 0.06),
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(theme.palette.grey[200], 0.12),
+    },
+		'.MuiChip-icon': {
+			color: theme.palette.grey[900]
+		}
+  };
+}) as typeof Chip;
+
+
