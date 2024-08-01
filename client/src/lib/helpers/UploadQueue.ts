b89e4be34f4e092e76ce1689fda2bce2122c5722
nxvhm <a.partadzhiev@gmail.com>
2024-08-01 23:04:11 +0300

    Upload queue

--- client/src/lib/helpers/UploadQueue.ts
+++ client/src/lib/helpers/UploadQueue.ts
@@ -1,28 +1,70 @@
-import { FileToUpload } from "../../definitions";
+import { FileModel, FileToUpload } from "../../definitions";
 
 class UploadQueue {
 
-	public static pendingUploads: {[key: string]: FileToUpload} = {};
+	public static pendingUploads = new Map<string, FileToUpload>;
+
+	/**
+	 * The ID of the internval that is looping through the pendingUploads
+	 */
 	public static uploadProcessHandler: number;
+
+	/**
+	 * Holds the hash of the currently uploading file.
+	 * This hash is a key of the pendingUploads object
+	 */
+	public static currentlyUploading: string | null;
+
+	/**
+	 * The actual upload implementation.
+	 * Should be provided from outside the class
+	 * and it should return a promise, since we need to know when the upload ends
+	 * so we can remove the file from the pending uploads
+	 */
+	public static uploadFunction: (file: FileToUpload) => Promise<FileModel>;
 
 	public static addPendingUploads(uploads: FileToUpload[]): void {
 		uploads.forEach((file: FileToUpload) => {
-			if(!this.pendingUploads.hasOwnProperty(file.hash))
-				this.pendingUploads[file.hash] = file;
+			if(!this.pendingUploads.has(file.hash))
+				this.pendingUploads.set(file.hash, file);
 		})
 
-		if(Object.keys(this.pendingUploads).length)
-				this.startUpload();
+		if(this.pendingUploads.size)
+			this.startUploadLoop();
 	}
 
-	public static startUpload = () => {
+	public static startUploadLoop = () => {
 		if(!this.uploadProcessHandler)
 			this.uploadProcessHandler = setInterval(this.processUploads, 1000)
 	}
 
-	public static processUploads = () => {
-		console.log('uploading....', this.pendingUploads);
+	public static stopUploadLoop = () => {
+		if(this.uploadProcessHandler)
+			clearInterval(this.uploadProcessHandler);
+
+		this.currentlyUploading = null;
 	}
+
+	public static processUploads = () => {
+		const iterator = this.pendingUploads.values();
+		const next = iterator.next().value;
+		if(!next)
+			return this.stopUploadLoop();
+
+		if(this.currentlyUploading == next.hash)
+			return;
+
+		this.currentlyUploading = next.hash;
+		this.uploadFunction(next).then(_uploadedFile => {
+			UploadQueue.pendingUploads.delete(next.hash);
+		})
+	}
+
+	public static isLooping = (): boolean => {
+		return Boolean(this.uploadProcessHandler);
+	}
+
+
 
 }
 
