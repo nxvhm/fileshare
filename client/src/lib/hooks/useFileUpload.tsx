de17acb976fde70c71eb4187d9116ce0e7dc9518
nxvhm <a.partadzhiev@gmail.com>
2024-08-02 22:20:39 +0300

    Use upload queue to upload files

--- client/src/lib/hooks/useFileUpload.tsx
+++ client/src/lib/hooks/useFileUpload.tsx
@@ -1,35 +1,27 @@
-import { useState, useEffect } from "react";
-import { FileModel } from "../../definitions";
+import { useState, useEffect, useMemo } from "react";
+import { FileModel, FileToUpload } from "../../definitions";
 import toast from "react-hot-toast";
 import { uploadFile as uploadFileRequest } from "../../api/Files";
 import { ListSubheader, List, ListItemText, ListItem, Slide, Chip} from '@mui/material';
 import { UploadDialogBox } from "../../components/files/styled";
-
-
-type fileToUpload = {
-	file: File,
-	status: 'pending' | 'uploaded'
-};
+import UploadQueue from "../helpers/UploadQueue";
 
 export default function useFileUpload(parentId: undefined|number) {
 	const [uploadedFile, setUploadedFile] = useState<FileModel|null>(null)
-	const [filesToUpload, setFilesToUpload] = useState<fileToUpload[]|null>(null);
+	const [filesToUpload, setFilesToUpload] = useState<FileToUpload[]|null>(null);
 
-	/**
-	 * Upload file to the server
-	 * @param files FilesList
-	 * @returns void
-	 */
-	const uploadFile = (files: FileList) => {
-		if(!files)
-			return;
-
+	const getUploadRequestFormData = (file: File): FormData => {
 		const formData = new FormData();
-		formData.append("file", files[0]);
+		formData.append("file", file);
 
 		if(parentId)
 			formData.set('parentId', String(parentId));
 
+		return formData;
+	}
+
+	const uploadFile = (file: File) => {
+		const formData = getUploadRequestFormData(file);
 		uploadFileRequest(formData)
 			.then(file => setUploadedFile(file))
 			.catch(e => {
@@ -42,30 +34,52 @@ export default function useFileUpload(parentId: undefined|number) {
 			})
 	}
 
-	/**
-	 * When files to upload list is updated
-	 */
+	const queueUploadHandler = (file: FileToUpload): Promise<FileModel> => {
+		const formData = getUploadRequestFormData(file.file);
+		return new Promise((resolve, reject) => {
+			uploadFileRequest(formData).then(uploadedFile => {
+				setUploadedFile(uploadedFile);
+				setFilesToUpload(filesToUpload => {
+					return !filesToUpload ? [] : filesToUpload.filter(fileToUpload => fileToUpload.hash != file.hash);
+				})
+				resolve(uploadedFile);
+			}).catch(e => {
+				toast.error(
+					e.response?.data
+						? e.response?.data
+						: (e instanceof Error ? e.message : 'Error Occured, please try again later'),
+					{duration: 3000, position: 'top-center'}
+				);
+				reject(e);
+			})
+		})
+	}
+
 	useEffect(() => {
 		if(!filesToUpload?.length)
 			return;
-
-		console.log(filesToUpload);
+		UploadQueue.addPendingUploads(filesToUpload);
 	}, [filesToUpload])
+
+	useEffect(() => {
+		UploadQueue.uploadFunction = queueUploadHandler;
+	}, [])
 
 	const addFilesToUpload = (files: File[]) => {
 		const currentFiles = filesToUpload ? [...filesToUpload] : [];
-		const incomingFiles = files.map((file: File): fileToUpload => {
+		const incomingFiles = files.map((file: File): FileToUpload => {
 				return {
 					file: file,
-					status: 'pending'
+					status: 'pending',
+					hash: (Math.random()).toString(16).substring(3, 10)
 				}
 		});
-
 		const merged = [...currentFiles.concat(incomingFiles)];
 		setFilesToUpload(merged);
 	}
 
-	const handleSelectedFile = (e: React.ChangeEvent<HTMLInputElement>) => e.target.files && uploadFile(e.target.files)
+
+	const handleSelectedFile = (e: React.ChangeEvent<HTMLInputElement>) => e.target.files && uploadFile(e.target.files[0])
 	const getFileToUploadDescription = (file: File): string => file.type + ' '+ file.size/1000 + ' KB';
 
 	const UploaderWidget = (): JSX.Element | undefined => {
@@ -73,7 +87,7 @@ export default function useFileUpload(parentId: undefined|number) {
 			return;
 
 		return(
-			<Slide direction="up" in={Boolean(filesToUpload)} mountOnEnter unmountOnExit timeout={250}>
+			<Slide direction="up" in={Boolean(filesToUpload)} timeout={250}>
 			<UploadDialogBox>
 				<List>
 					<ListSubheader component="div">
@@ -81,7 +95,7 @@ export default function useFileUpload(parentId: undefined|number) {
         	</ListSubheader>
 				{filesToUpload.map((item) => {
 				return (
-					<ListItem key={item.file.lastModified}>
+					<ListItem key={(Math.random()).toString(16)}>
 						 <ListItemText secondary={getFileToUploadDescription(item.file)}>
 								<Chip label={item.status.toUpperCase()} size='small' color='warning' /> {item.file.name}
 							</ListItemText>
