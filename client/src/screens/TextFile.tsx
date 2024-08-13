052484596a2349f0d5192d170b721b8075682aa9
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-08-13 22:27:27 +0300

    Display basic txt editor

--- client/src/screens/TextFile.tsx
+++ client/src/screens/TextFile.tsx
@@ -1,7 +1,20 @@
-const TextFile = props => {
-	return(
-		<p>Text file view</p>
-	)
+import { useState } from 'react';
+import Editor from 'react-simple-wysiwyg';
+
+const TextFile = () => {
+  const [html, setHtml] = useState('my <b>HTML</b>');
+
+  function onChange(e) {
+    setHtml(e.target.value);
+  }
+
+  return (
+    <Editor
+			containerProps={{ style: { resize: 'vertical' } }}
+			value={html}
+			onChange={onChange}
+		/>
+  );
 }
 
 export default TextFile
